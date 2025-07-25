7ce92ee17e9a33707686c68830e5ffe3e21c98b1
Ilia Bubnov <makebreathe1224@gmail.com>
2025-07-25 19:51:56 +0200

    ran prettier and did a double check of some parameters

--- index.ts
+++ index.ts
@@ -1,37 +1,39 @@
-import { PrismaClient } from './generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import {faker} from "@faker-js/faker";
+import { PrismaClient } from "./generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { faker } from "@faker-js/faker";
 
-const prisma = new PrismaClient()
-    .$extends(withAccelerate())
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 async function main() {
-    for (let i = 0; i < 100; i++) {
-        const firstName = faker.person.firstName()
-        const lastName = faker.person.lastName()
-        const username = faker.internet.username({firstName: firstName, lastName: lastName})
-        const email = username + '@gmail.com'
+  for (let i = 0; i < 100; i++) {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const username = faker.internet.username({
+      firstName: firstName,
+      lastName: lastName,
+    });
+    const email = username + "@gmail.com";
 
-        const user = await prisma.user.create({
-            data: {
-                username: username,
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                avatarURL: faker.image.avatar(),
-                bio: faker.lorem.paragraph({ min: 1, max: 5 }),
-                createdAt: faker.date.recent({ days: 30 })
-            }
-        })
-    }
+    const user = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        avatarURL: faker.image.avatar(),
+        bio: faker.lorem.paragraph({ min: 1, max: 5 }),
+        createdAt: faker.date.recent({ days: 30 }),
+      },
+    });
+  }
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
